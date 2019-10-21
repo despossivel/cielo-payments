c56dd88f7e4bf1d41c89a47df9a4a23291eb558f
despossivel <mattbmoller@gmail.com>
2019-10-21 20:25:28 -0300

    alinhamento

---
+++ README.md
@@ -0,0 +1,145 @@
+#################################################################################
+ transacao simples credito
+#################################################################################
+
+
+ var dadosSale = {  
+    "MerchantOrderId":"2014111703",
+    "Customer":{  
+       "Name":"Comprador crédito simples"
+    },
+    "Payment":{  
+      "Type":"CreditCard",
+      "Amount":15700,
+      "Installments":1,
+      "SoftDescriptor":"123456789ABCD",
+      "CreditCard":{  
+          "CardNumber":"0000000000000001",
+          "Holder":"Teste Holder",
+          "ExpirationDate":"12/2030",
+          "SecurityCode":"123",
+          "Brand":"Visa"
+      }
+    }
+ }
+
+ Usando Promise
+ cielo.creditCard.simpleTransaction(dadosSale).then((data) => {
+         return console.log("PaymentId Credito: "+data.Payment.PaymentId);
+     })
+     .catch((err) => {
+         return console.error('ERRO', err);
+     });
+
+
+
+
+Ou usando Async / Await
+const transaction = await cielo.creditCard.simpleTransaction(dadosSale);
+console.log(transaction);
+#################################################################################
+
+
+#################################################################################
+transacao completa credito
+usando promise
+ cielo.creditCard.completeTransaction(dadosSale)
+     .then((data) => {
+         return console.log(data);
+     })
+     .catch((err) => {
+         return console.error('ERRO', err);
+     })
+ })
+
+Ou usando Async / Await
+const transaction = await cielo.creditCard.completeTransaction(dadosSale);
+console.log(transaction);
+#################################################################################
+#################################################################################
+
+
+
+
+
+
+#################################################################################
+transacao simples DEBITO
+#################################################################################
+ var dadosSale = {  
+    "MerchantOrderId":"2014121201",
+    "Customer":{  
+       "Name":"Comprador Cartão de débito"
+    },
+    "Payment":{  
+      "Type":"DebitCard",
+      "Amount":15700,
+      "ReturnUrl":"http:www.cielo.com.br",
+      "DebitCard":{  
+          "CardNumber":"4551870000000183",
+          "Holder":"Teste Holder",
+          "ExpirationDate":"12/2030",
+          "SecurityCode":"123",
+          "Brand":"Visa"
+      }
+    }
+ }
+
+ cielo.debitCard.simpleTransaction(dadosSale)
+     .then((data) => {
+         return console.log("PaymentId Debito: "+data.Payment.PaymentId);
+     })
+     .catch((err) => {
+         return console.error('ERRO', err);
+     });
+
+Ou usando Async / Await
+ const transaction = await cielo.debitCard.simpleTransaction(dadosSale);
+ console.log(transaction);
+#################################################################################
+#################################################################################
+
+ Capturando uma venda
+ #################################################################################
+
+ var dadosSale = {
+     paymentId: '01df6e28-6ddd-45db-a095-903c1adb170a',
+     amount: '15700'
+ }
+
+ cielo.creditCard.captureSaleTransaction(dadosSale)
+     .then((data) => {
+         return console.log(data);
+     })
+     .catch((err) => {
+         return console.error('ERRO', err);
+     })
+ })
+
+Ou usando Async / Await
+const transaction = await cielo.creditCard.captureSaleTransaction(dadosSale);
+console.log(transaction);
+ #################################################################################
+ #################################################################################
+
+
+Cancelando uma venda
+#################################################################################
+ var dadosSale = {
+     paymentId: '01df6e28-6ddd-45db-a095-903c1adb170a',
+     amount: '15700'
+ }
+
+ cielo.creditCard.cancelSale(dadosSale)
+     .then((data) => {
+         return console.log(data);
+     })
+     .catch((err) => {
+         return console.error('ERRO', err);
+     })
+ })
+Ou usando Async / Await
+ const cancel = await cielo.creditCard.cancelSale(dadosSale);
+ console.log(cancel);
+#################################################################################
+#################################################################################
--- processar.js
+++ processar.js
@@ -1,111 +1,89 @@
-let id = '';
-let key = '';
-let paramsCielo = {
-    'MerchantId': id,
-    'MerchantKey': key,
-   // 'RequestId': 'xxxxxxx', // Opcional - Identificação do Servidor na Cielo
-    'sandbox': true, // Opcional - Ambiente de Testes
-   // 'debug': true // Opcional - Exibe os dados enviados na requisição para a Cielo
-};
- 
-var cielo = require('cielo')(paramsCielo);
+const id = '',
+	key = '',
+	paramsCielo = {
+		'MerchantId': id,
+		'MerchantKey': key,
+		'sandbox': true,
+	},
+	cielo = require('cielo')(paramsCielo);
 
 module.exports = class processar {
-	constructor(json){
-		 this.MerchantOrderId = json.MerchantOrderId;
-		 this.CustomerName = json.CustomerName; 
-		 this.Type = json.type;
-		 this.Amount = json.Amount;
-		 this.CardNumber = json.CardNumber;
-		 this.Holder = json.Holder;
-		 this.ExpirationDate = json.ExpirationDate;
-		 this.SecurityCode = json.SecurityCode;
-		 this.Brand = json.Brand;
-		 this.Installments;
-		 this.SoftDescriptor;
-		  this.dadosSale = { "MerchantOrderId":this.MerchantOrderId,
-							 "Customer":{  
-							    "Name":this.CustomerName
-							   },
-							   "Payment":{  
-							     "Type":this.Type,
-							     "Amount":this.Amount,
-							   }
-							}	 
- 					 
-		 	switch(this.Type){
-		 		case 'CreditCard':
-		 		  this.dadosSale.Payment.CreditCard = {  
-											         "CardNumber":this.CardNumber,
-											         "Holder":this.Holder,
-											         "ExpirationDate":this.ExpirationDate,
-											         "SecurityCode":this.SecurityCode,
-											         "Brand":this.Brand
-											     }
-		 		break;
+	constructor(json) {
+		this.MerchantOrderId = json.MerchantOrderId;
+		this.CustomerName = json.CustomerName;
+		this.Type = json.type;
+		this.Amount = json.Amount;
+		this.CardNumber = json.CardNumber;
+		this.Holder = json.Holder;
+		this.ExpirationDate = json.ExpirationDate;
+		this.SecurityCode = json.SecurityCode;
+		this.Brand = json.Brand;
+		this.Installments;
+		this.SoftDescriptor;
+		this.dadosSale = {
+			"MerchantOrderId": this.MerchantOrderId,
+			"Customer": {
+				"Name": this.CustomerName
+			},
+			"Payment": {
+				"Type": this.Type,
+				"Amount": this.Amount,
+			}
+		}
 
-		 		case 'DebitCard':
-		 		 this.dadosSale.Payment.DebitCard = {  
-											         "CardNumber":this.CardNumber,
-											         "Holder":this.Holder,
-											         "ExpirationDate":this.ExpirationDate,
-											         "SecurityCode":this.SecurityCode,
-											         "Brand":this.Brand
-											     }
-		 		break;
-		 	}
-	
-	}
-
-	execultar(){
-
-		switch(this.Type){
+		switch (this.Type) {
 			case 'CreditCard':
+				this.dadosSale.Payment.CreditCard = {
+					"CardNumber": this.CardNumber,
+					"Holder": this.Holder,
+					"ExpirationDate": this.ExpirationDate,
+					"SecurityCode": this.SecurityCode,
+					"Brand": this.Brand
+				}
+				break;
 
-				this.dadosSale.Payment.Installments = 1;
-				this.dadosSale.Payment.SoftDescriptor = "Pinpper";
-
-				cielo.creditCard.simpleTransaction(this.dadosSale).then((data) => {
-					return console.log("PaymentId Credito: "+data.Payment.PaymentId);
-				})
-				.catch((err) => {
-					return console.error('ERRO', err);
-				});
-
-			break;
 			case 'DebitCard':
-
-				this.dadosSale.Payment.ReturnUrl = "http://www.cielo.com.br";
-
-				cielo.debitCard.simpleTransaction(this.dadosSale)
-				.then((data) => {
-						return console.log("PaymentId Debito: "+data.Payment.PaymentId);
-				})
-				.catch((err) => {
-					return console.error('ERRO', err);
-				});
-
-			break;
+				this.dadosSale.Payment.DebitCard = {
+					"CardNumber": this.CardNumber,
+					"Holder": this.Holder,
+					"ExpirationDate": this.ExpirationDate,
+					"SecurityCode": this.SecurityCode,
+					"Brand": this.Brand
+				}
+				break;
 		}
 
 	}
 
-	visualizar(){
-		
-		var dadosSale = {
+	async execultar() {
+
+		switch (this.Type) {
+			case 'CreditCard':
+
+				this.dadosSale.Payment.Installments = 1;
+				this.dadosSale.Payment.SoftDescriptor = "Pinpper"
+				await cielo.creditCard.simpleTransaction(this.dadosSale).catch(err => console.error('ERRO', err));
+
+				break;
+			case 'DebitCard':
+
+				this.dadosSale.Payment.ReturnUrl = "http://www.cielo.com.br";
+				await cielo.debitCard.simpleTransaction(this.dadosSale).catch((err) => console.error('ERRO', err));
+
+				break;
+		}
+
+	}
+
+	async visualizar() {
+
+		const dadosSale = {
 			paymentId: '01df6e28-6ddd-45db-a095-903c1adb170a',
 			amount: '15700'
 		}
 
-		cielo.creditCard.captureSaleTransaction(dadosSale)
-		    .then((data) => {
-		        return console.log(data);
-		    })
-		    .catch((err) => {
-		        return console.error('ERRO', err);
-		    });
-
+		await cielo.creditCard.captureSaleTransaction(dadosSale).catch((err) => console.error('ERRO', err));
 
 	}
-  
+
 }
